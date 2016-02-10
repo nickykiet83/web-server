addce70be56bd3db5a78271aa9eccf3a54f4f27e
Nickykiet83 <phamquanganhkiet@gmail.com>
2016-02-10 23:33:44 +0700

    Update language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(middleware.logger);
 
 //about url
 app.get('/about', middleware.requireAuthentication, function(req, res){
-    res.send("About page");
+    res.send("About page!");
 });
 
 app.use(express.static(__dirname + '/public'));
